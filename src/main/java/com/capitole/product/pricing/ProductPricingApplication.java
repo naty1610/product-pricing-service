fd5cbe5632053b2b7b4a9c69b3de56618f1f8ac6
Daury <daurylecca16@gmail.com>
2025-03-12 06:31:26 -0500

    Scaffolding inicial

---
+++ src/main/java/com/capitole/product/pricing/ProductPricingApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.product.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductPricingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductPricingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitole/product/pricing/ProductPricingApplicationTest.java
@@ -0,0 +1,13 @@
+package com.capitole.product.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductPricingApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
